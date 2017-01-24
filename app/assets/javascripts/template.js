a779f2a8b58009d4dedb7cd175c3495d9671f530
miffyC <miffy.dandelion@gmail.com>
2017-01-24 15:30:24 +0800

    fadeIn

--- app/assets/javascripts/template.js
+++ app/assets/javascripts/template.js
@@ -360,7 +360,7 @@
 			});
 		};
 
-		
+
 		// Isotope filters
 		//-----------------------------------------------
 		if ($('.isotope-container').length>0 || $('.masonry-grid').length>0 || $('.masonry-grid-fitrows').length>0) {
@@ -372,13 +372,6 @@
 				$('.masonry-grid-fitrows').isotope({
 					itemSelector: '.masonry-grid-item',
 					layoutMode: 'fitRows'
-				});
-				$('.isotope-container').fadeIn();
-				var $container = $('.isotope-container').isotope({
-					itemSelector: '.isotope-item',
-					layoutMode: 'masonry',
-					transitionDuration: '0.6s',
-					filter: "*"
 				});
 				// filter items on button click
 				$('.filters').on( 'click', 'ul.nav li a', function() {
