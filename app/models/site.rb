30137842e3f2458734ee1457b15d537673d95c12
billy3321 <billy3321@gmail.com>
2021-08-16 17:51:03 +0800

    add site

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,10 +1,9 @@
-class Slide < ApplicationRecord
+class Site < ApplicationRecord
   mount_uploader :image, ImageUploader
-  belongs_to :slideable, polymorphic: true
   default_scope { order(position: :asc) }
   before_save :set_position
 
   def set_position
-    self.position ||= Slide.maximum("position").to_i + 1
+    self.position ||= Site.maximum("position").to_i + 1
   end
 end
