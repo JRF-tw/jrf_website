9745f8cbf0f0394b950f1b404dd9abbf17950650
billy3321 <billy3321@gmail.com>
2025-07-28 13:58:54 +0800

    fix 404 error

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,33 +1,28 @@
 class ErrorsController < ApplicationController
   def error404
     @keyword = Keyword.where(name: '反黑箱服貿案').first
-    if json_request?
-      render json: {error: "not found", status: "failed"}, status: :not_found, callback: params[:callback]
-    else
-      render status: :not_found
+    respond_to do |format|
+      format.html { render status: :not_found }
+      format.json { render json: {error: "not found", status: "failed"}, status: :not_found, callback: params[:callback] }
+      format.any { render status: :not_found, formats: [:html] }
     end
   end
 
   def error422
-    if json_request?
-      render json: {error: "unprocessable entity", status: "failed"}, status: :unprocessable_entity, callback: params[:callback]
-    else
-      render status: :unprocessable_entity
+    respond_to do |format|
+      format.html { render status: :unprocessable_entity }
+      format.json { render json: {error: "unprocessable entity", status: "failed"}, status: :unprocessable_entity, callback: params[:callback] }
+      format.any { render status: :unprocessable_entity, formats: [:html] }
     end
   end
 
   def error500
     @keyword = Keyword.where(name: '邱和順案').first
-    if json_request?
-      render json: {error: "internal server error", status: "failed"}, status: :internal_server_error, callback: params[:callback]
-    else
-      render status: :internal_server_error
+    respond_to do |format|
+      format.html { render status: :internal_server_error }
+      format.json { render json: {error: "internal server error", status: "failed"}, status: :internal_server_error, callback: params[:callback] }
+      format.any { render status: :internal_server_error, formats: [:html] }
     end
   end
 
-  private
-
-  def json_request?
-    request.env['ORIGINAL_FULLPATH'] =~ /\.json$/
-  end
 end
